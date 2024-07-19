41cbd653de9bd082aa2ed0e9638d9d78c3ea4442
AndrewBerdyshev <an.berdyshev@gmail.com>
2024-07-19 13:33:57 +0700

    Reapply "shadow VMT hooking added."
    This reverts commit f8663df056c680e3c9f2ef40276a06bd3e56fa89.

--- mylib/src/mylib.h
+++ mylib/src/mylib.h
@@ -9,5 +9,26 @@ namespace mylib
 	size_t GetFuncSize(void* func);
 	uint32_t GetProcessID(const char* processName);
 	uint32_t GetThreadID(uint32_t processID);
-	uint8_t* ReadFile(const char* fileName);
+	uint8_t* MyReadFile(const char* fileName);
+
+	namespace shadowVMT
+	{
+		class VMT
+		{
+		private:
+			uintptr_t* originalVMT;
+			uintptr_t* shadowVMT;
+			uintptr_t* instanceVMT;
+		public:
+			VMT(uintptr_t* classInstance);
+			~VMT();
+			uintptr_t Hook(uint64_t index, uintptr_t hookFunc);
+			template<typename Result, typename ... Args>
+			Result Call(uint64_t index, Args ... args)
+			{
+				using Fn = Result(__thiscall*)(void*, decltype(Args)...);
+				reinterpret_cast<Fn*>(originalVMT)[index](originalVMT, args...);
+			}
+		};
+	}
 }
